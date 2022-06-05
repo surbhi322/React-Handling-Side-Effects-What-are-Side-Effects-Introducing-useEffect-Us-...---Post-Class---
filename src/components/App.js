1801340b9b81042e47b2737301770f7a91b486f7
surbhi322 <81092552+surbhi322@users.noreply.github.com>
2022-06-05 22:05:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,52 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
-
-  return (
+  const [count, setCount] = useState(0);
+  const [timeOn, setTimeON] = useState(false);
+  
+  useEffect(()=>{
+    let interval = null;
+   if(timeOn){
+     interval = setInterval(()=>{
+       setCount(prevTime => prevTime - 1);
+     },1000)
+   }else{
+     clearInterval(interval);
+   }
+   return ()=> clearInterval(interval);
+  },[timeOn])
+ 
+   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={(event)=>{
+          let val = Number(event.target.value);
+          console.log(typeof val)
+          val = Math.floor(val);
+          if(isNaN(val)){
+            if(event.key === 'Enter'){
+              setTimeON(false);
+              setCount(0);
+           }
+          }else{
+            if(event.key === 'Enter'){
+              if(val === 0){
+                setCount(0);
+                setTimeON(false);
+              }else{
+                setTimeON(true);
+                setCount(val);
+              }
+              
+           }
+          }
+
+          }} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{count}</div>
     </div>
   )
 }
-
-
 export default App;
